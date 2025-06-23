dd77e951059264f3ca22c23d5a9e4b926bf26f3e
capt-farvez <farvez.capt@gmail.com>
2025-06-23 16:15:34 +0600

    Add hero,  testimonials, and features sections with responsive design. Enhance navbar styles when scroll to add background color.

--- Projects/05-Leno-Website/js/script.js
+++ Projects/05-Leno-Website/js/script.js
@@ -6,3 +6,15 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileMenu.classList.toggle('active');
     });
 });
+
+// Navbar background color on scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar--scroll');
+    } 
+    else {
+        navbar.classList.remove('navbar--scroll');
+    }
+});
