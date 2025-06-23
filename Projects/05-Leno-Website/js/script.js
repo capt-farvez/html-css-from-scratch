b196883554824d0af439c0f3329f50b18937b3fb
capt-farvez <farvez.capt@gmail.com>
2025-06-24 00:30:31 +0600

    Add preview section with video modal functionality using javascript, Screenshots section, Download section, Details section, and footer to index.html and styles.css

--- Projects/05-Leno-Website/js/script.js
+++ Projects/05-Leno-Website/js/script.js
@@ -5,6 +5,35 @@ document.addEventListener('DOMContentLoaded', function () {
     toggleButton.addEventListener('click', function () {
         mobileMenu.classList.toggle('active');
     });
+
+    /* Video Modal when button click open video */
+    const modal = document.getElementById('videoModal');
+    const videoButton = document.querySelector('.preview__video-button');
+    const closeButton = document.querySelector('.modal__close-button');
+    const videoPlayer = document.getElementById('videoPlayer');
+
+    /* Open modal when clicked */
+    videoButton.addEventListener('click', function () {
+        /* Show modal */
+        modal.style.display = 'block';
+
+        videoPlayer.src = 'https://www.youtube.com/embed/a040VmmO-AY';
+
+        /* Close modal when close button click */
+        closeButton.addEventListener('click', function () {
+            modal.style.display = 'none';
+            videoPlayer.src = '';
+        });
+
+        /* Close modal when outter click */
+        window.addEventListener('click', function (event) {
+            if(event.target == modal) {
+                modal.style.display = 'none';
+                videoPlayer.src = '';
+            }
+        });
+    });
+  
 });
 
 // Navbar background color on scroll
